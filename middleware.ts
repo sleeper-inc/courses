69441f0912c7f49226c2caf5f13f110ce7174dee
dikakoko <dikakoko04@gmail.com>
2024-04-09 12:49:15 +0700

    Fix middleware

--- middleware.ts
+++ middleware.ts
@@ -11,5 +11,5 @@ export const config = {
     // Protects all routes, include api/trpc.
     // See https://clerk.com/docs/references/nextjs/auth-middleware
     // for more information about configuring your middleware
-    matcher: ["/((?!.+\\.\\[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
